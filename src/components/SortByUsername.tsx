144f952b571304f5fbafd628264e70434040176e
Ivan Gonzalez <ivanezeq.gonzalez@gmail.com>
2025-05-25 18:39:16 -0300

    se actualizan estilos y ajustes para componente ordenamiento y su modal.

--- src/components/SortByUsername.tsx
+++ src/components/SortByUsername.tsx
@@ -9,6 +9,8 @@ import {
   Modal,
 } from 'react-native';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Feather from 'react-native-vector-icons/Feather';
+
 import {FilterBy, Order} from '../screens/HomeScreen';
 
 export type SettingResults = {
@@ -73,25 +75,37 @@ export const SortByUsername = (props: ISortByUserName) => {
     // TODO: agregar seccion Filtrar por favorito y otra para ordernar por asc desc. Tambien poner titulo de Configuración de resultados de búsqueda
     <View style={styles.container}>
       <Modal visible={open} animationType="slide">
-        <Text>Filtrar por</Text>
-        <View>
-          <Text>Por Favorito</Text>
-          <Switch
-            value={temporalFilter.filters.byFavorite}
-            onValueChange={value => updateTemporalFilter('byFavorite', value)}
-          />
-          <Text>Ascendete | Descendente</Text>
-          <ToggleOrder
-            order={temporalFilter.order}
-            toggleOrder={() => updateTemporalOrder()}
-          />
+        <View style={styles.modalContainer}>
+          <Text style={styles.title}>Filtros y ordenamiento</Text>
+          <View style={styles.containerFilters}>
+            <View style={styles.comboLabelValue}>
+              <Text style={styles.labelSetting}>Por Favorito</Text>
+              <Switch
+                value={temporalFilter.filters.byFavorite}
+                onValueChange={value =>
+                  updateTemporalFilter('byFavorite', value)
+                }
+              />
+            </View>
+            <View style={styles.comboLabelValue}>
+              <Text style={styles.labelSetting}>Ascendete | Descendente</Text>
+              <ToggleOrder
+                order={temporalFilter.order}
+                toggleOrder={() => updateTemporalOrder()}
+              />
+            </View>
+          </View>
+          <View style={styles.containerButtons}>
+            <Button title="Guardar" onPress={successModal} color={'#27ae60'} />
+            <Button title="Cancelar" onPress={dimissModal} color={'#FFC300'} />
+          </View>
         </View>
-        <Button title="Guardar" onPress={successModal} />
-        <Button title="Cancelar" onPress={dimissModal} />
       </Modal>
-      <Text style={styles.text}>Ordenamiento por username</Text>
-      <Button title="filters" onPress={openModal} />
+      <Text style={styles.text}>Filtros y ordenamiento</Text>
       <ToggleOrder order={settingsResults.order} toggleOrder={toggleOrder} />
+      <TouchableOpacity onPress={openModal}>
+        <Feather name="filter" color="#000" size={24} />
+      </TouchableOpacity>
     </View>
   );
 };
@@ -105,9 +119,46 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     paddingVertical: 10,
     paddingHorizontal: 10,
+    gap: 20,
+  },
+  comboLabelValue: {
+    backgroundColor: 'pink',
+    borderRadius: 8,
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    paddingHorizontal: 10,
+    height: 36,
+    alignItems: 'center',
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: 700,
+  },
+  modalContainer: {
+    height: '100%',
+    flexDirection: 'column',
+    gap: 10,
+    padding: 15,
+  },
+  containerFilters: {
+    flex: 1,
+    padding: 10,
+    gap: 8,
+  },
+  labelSetting: {
+    fontSize: 17,
+    color: '#020202',
+    fontWeight: 300,
+  },
+  containerButtons: {
+    justifyContent: 'space-around',
+    flexDirection: 'row',
+    alignContent: 'flex-end',
   },
   text: {
     fontWeight: '400',
+    flex: 1,
   },
 });
 
